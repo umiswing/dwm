99c571ca9d6a9a57eecd398bf193ede247bfd4c7
umiswing <umiswing@foxmail.com>
2023-10-13 10:10:51 +0800

    Not keeping scratchpad in center.

--- dwm.c
+++ dwm.c
@@ -1641,10 +1641,11 @@ showhide(Client *c)
 	if (!c)
 		return;
 	if (ISVISIBLE(c)) {
-		if ((c->tags & SPTAGMASK) && c->isfloating) {
-			c->x = c->mon->wx + (c->mon->ww / 2 - WIDTH(c) / 2);
-			c->y = c->mon->wy + (c->mon->wh / 2 - HEIGHT(c) / 2);
-		}
+                // Uncomment following to keep scratchpad in center.
+		/* if ((c->tags & SPTAGMASK) && c->isfloating) { */
+		/* 	c->x = c->mon->wx + (c->mon->ww / 2 - WIDTH(c) / 2); */
+		/* 	c->y = c->mon->wy + (c->mon->wh / 2 - HEIGHT(c) / 2); */
+		/* } */
 		/* show clients top down */
 		XMoveWindow(dpy, c->win, c->x, c->y);
 		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
